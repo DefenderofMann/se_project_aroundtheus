d30c864c9d75e8f16d53066ab5d73ada8595e17e
Logan Pryor <pryor60123@gmail.com>
2024-04-17 22:53:45 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,25 @@
 const initialCards = [
-  {
+  let object_1 ={
     title: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
-  {
+   let object_2 = {
     title: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
-  {
+   let object_3 = {
     title: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
-  {
+   let object_4 = {
     title: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
-  {
+let object_5 ={
     title: "Vanoise National Park ",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
-  {
+  let object_6 ={
     title: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
